ae2912c589f81435801cd811a84ce0299810c15a
Hero Protagonist <heroprotagonist32@gmail.com>
2025-04-23 22:25:45 -0400

    fmt and some logic

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import pandiff from 'pandiff'
  */
 export async function run(): Promise<void> {
   try {
-    
     const token = core.getInput('repo-token')
     const octokit = github.getOctokit(token)
     const context = github.context
@@ -19,32 +18,47 @@ export async function run(): Promise<void> {
       return
     }
 
-    const { owner, repo } = context.repo;
-    const pull_number = context.payload.pull_request.number;
-    const baseBranch = context.payload.pull_request.base.ref;
+    const { owner, repo } = context.repo
+    const pull_number = context.payload.pull_request.number
+    const baseBranch = context.payload.pull_request.base.ref
 
-    core.info(`🔍 Fetching changed files for PR #${pull_number} against ${baseBranch}`);
+    core.info(
+      `🔍 Fetching changed files for PR #${pull_number} against ${baseBranch}`
+    )
 
     const prFiles = await octokit.paginate(
       octokit.rest.pulls.listFiles,
       { owner, repo, pull_number, per_page: 100 },
       (response) => response.data
-    );
+    )
 
     const pandiffedFiles = []
-
-    for(const file of prFiles){
+    for (const file of prFiles) {
       try {
         const prRawText = await (await fetch(file.raw_url)).text()
-        const urlBase = file.raw_url.split('/raw/')[0] + '/raw/' + baseBranch + '/' + file.filename
-        const baseRawText = await(await fetch(urlBase)).text()
-        const diff = pandiff(baseRawText, prRawText, {to:'html'})
-        pandiffedFiles.push({file: file.filename, diff})
+        const urlBase =
+          file.raw_url.split('/raw/')[0] +
+          '/raw/' +
+          baseBranch +
+          '/' +
+          file.filename
+        const baseRawText = await new Promise<string>(async (resolve) => {
+          try {
+            resolve(await (await fetch(urlBase)).text())
+          } catch (error) {
+            core.error(`File ${file.filename} does not exist in ${baseBranch}`)
+            // try previous filename
+            // if that doesn't work, resolve to default ''
+            resolve('')
+          }
+        })
+        const diff = pandiff(baseRawText, prRawText, { to: 'html' })
+        pandiffedFiles.push({ file: file.filename, diff })
       } catch (error) {
         core.error(`File ${file.filename} fail. Skipped. Error: ${error}`)
       }
     }
-    core.setOutput('diffs', pandiffedFiles)    
+    core.setOutput('diffs', pandiffedFiles)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
